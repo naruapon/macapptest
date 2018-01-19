9d97cabdd38057c817f7cfc8d285f02b2f9e5f1f
Software Engineering <softwareengineering@Softwares-MacBook-Pro.local>
2018-01-20 02:01:52 +0700

    Initial Commit

---
+++ macapptest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  macapptest
+//
+//  Created by Software Engineering on 20/1/2561 BE.
+//  Copyright © 2561 Software Engineering. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ macapptest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  macapptest
+//
+//  Created by Software Engineering on 20/1/2561 BE.
+//  Copyright © 2561 Software Engineering. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
