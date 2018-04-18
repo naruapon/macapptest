3fc9b63c90cc986e3cf213a1640c27fb7fb02c21
naruapon <narauapon@gmail.com>
2018-04-18 22:52:01 +0700

    Initial commit

--- macapptest/ViewController.swift
+++ macapptest/ViewController.swift
@@ -10,12 +10,20 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+
+    @IBOutlet weak var number: NSTextField!
+    var count:Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func CountNumber(_ sender: Any) {
+        count += 1
+        number.stringValue = "\(count)"
+    }
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
